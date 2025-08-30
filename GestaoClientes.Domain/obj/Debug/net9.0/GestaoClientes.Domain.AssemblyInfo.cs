e6b7a97a80344b0f87b029bf371aac56b2f38439
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-08-29 21:08:04 -0300

    recompilado projeto

--- GestaoClientes.Domain/obj/Debug/net9.0/GestaoClientes.Domain.AssemblyInfo.cs
+++ GestaoClientes.Domain/obj/Debug/net9.0/GestaoClientes.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoClientes.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b1445ff62ecb14d42d4de46764627c4c70993c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a18315693011f63eff99b5866e403e5f4e689a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoClientes.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoClientes.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
